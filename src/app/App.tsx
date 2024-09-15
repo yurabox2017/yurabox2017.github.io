d6970d66080485862559d0469b7496c7c01927ae
Yurabox <yurabox85@mail.ru>
2024-09-15 12:41:54 +0300

    о себе

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,9 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
-        </p>
+        <ul className='text-left'>
+          <li> В компании по импортозамещению необходимо переписать проект с Blazor на React</li>
+          <li> Владею Blazor, AspNet, net6</li>
+          <li> FullStack разработчик, разрабатываю информационно-аналитическую систему. </li>
+          <li> 2022-2023гг проходил обучение по курсу AspNet core Otus.</li>
+        </ul>
       </header>
     </div>
   );
