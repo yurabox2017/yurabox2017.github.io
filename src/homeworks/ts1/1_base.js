367d4317317d2e69cc758752c7611c799620afd5
Yurabox <yurabox85@mail.ru>
2024-09-18 20:50:00 +0300

    Revert "typescript домашняя работа 1"
    This reverts commit ebb7be15fdc18cead77963c8d722cf156bb884bc.

--- src/homeworks/ts1/1_base.js
+++ src/homeworks/ts1/1_base.js
@@ -1,16 +1,16 @@
 /**
  * Нужно превратить файл в ts и указать типы аргументов и типы возвращаемого значения
  * */
-export const removePlus = (string: string): string => string.replace(/^\+/, '');
+export const removePlus = (string) => string.replace(/^\+/, '');
 
-export const addPlus = (string: string): string => `+${string}`;
+export const addPlus = (string) => `+${string}`;
 
-export const removeFirstZeros = (value: string): string => value.replace(/^(-)?[0]+(-?\d+.*)$/, '$1$2');
+export const removeFirstZeros = (value) => value.replace(/^(-)?[0]+(-?\d+.*)$/, '$1$2');
 
-export const getBeautifulNumber = (value: number, separator: string = ' '): string =>
+export const getBeautifulNumber = (value, separator = ' ') =>
   value?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
 
-export const round = (value: number, accuracy: number = 2): number => {
+export const round = (value, accuracy = 2) => {
   const d = 10 ** accuracy;
   return Math.round(value * d) / d;
 };
@@ -18,7 +18,7 @@ export const round = (value: number, accuracy: number = 2): number => {
 const transformRegexp =
   /(matrix\(-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, )(-?\d+(\.\d+)?), (-?\d+(\.\d+)?)\)/;
 
-export const getTransformFromCss = (transformCssString: string): { x: number, y: number } => {
+export const getTransformFromCss = (transformCssString) => {
   const data = transformCssString.match(transformRegexp);
   if (!data) return { x: 0, y: 0 };
   return {
@@ -27,20 +27,20 @@ export const getTransformFromCss = (transformCssString: string): { x: number, y:
   };
 };
 
-export const getColorContrastValue = ([red, green, blue]: [number, number, number]): number =>
+export const getColorContrastValue = ([red, green, blue]) =>
   // http://www.w3.org/TR/AERT#color-contrast
   Math.round((red * 299 + green * 587 + blue * 114) / 1000);
 
-export const getContrastType = (contrastValue: number): string => (contrastValue > 125 ? 'black' : 'white');
+export const getContrastType = (contrastValue) => (contrastValue > 125 ? 'black' : 'white');
 
 export const shortColorRegExp = /^#[0-9a-f]{3}$/i;
 export const longColorRegExp = /^#[0-9a-f]{6}$/i;
 
-export const checkColor = (color: string): void => {
+export const checkColor = (color) => {
   if (!longColorRegExp.test(color) && !shortColorRegExp.test(color)) throw new Error(`invalid hex color: ${color}`);
 };
 
-export const hex2rgb = (color: string): [number, number, number] => {
+export const hex2rgb = (color) => {
   checkColor(color);
   if (shortColorRegExp.test(color)) {
     const red = parseInt(color.substring(1, 2), 16);
@@ -54,23 +54,12 @@ export const hex2rgb = (color: string): [number, number, number] => {
   return [red, green, blue];
 };
 
-export const getNumberedArray = (arr: [number]): any => arr.map((value, number) => ({ value, number }));
-export const toStringArray = (arr: []) => arr.map(({ value, number }) => `${value}_${number}`);
+export const getNumberedArray = (arr) => arr.map((value, number) => ({ value, number }));
+export const toStringArray = (arr) => arr.map(({ value, number }) => `${value}_${number}`);
 
-
-type Key = {
-  id: number;
-}
-type Customer = {
-  name: string,
-  age: number,
-  isSubscribed: boolean
-}
-type CustomerKey = Key & Customer;
-
-export const transformCustomers = (customers: CustomerKey[]) => {
+export const transformCustomers = (customers) => {
   return customers.reduce((acc, customer) => {
     acc[customer.id] = { name: customer.name, age: customer.age, isSubscribed: customer.isSubscribed };
     return acc;
-  }, {} as Customer[]);
+  }, {});
 };
