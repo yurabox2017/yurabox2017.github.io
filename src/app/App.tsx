30dda289ce375ea301993b483e07b83afdfbfb55
Yurabox <yurabox85@mail.ru>
2024-09-25 21:35:46 +0300

    тест

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <ul className='text-left'>
-          <li> В компании по импортозамещению необходимо переписать проект с Blazor на React</li>
+          <li> В компании по импортозамещению необходимо переписать проект с Blazor на React.</li>
           <li> Владею Blazor, AspNet, net6</li>
           <li> FullStack разработчик, разрабатываю информационно-аналитическую систему. </li>
           <li> 2022-2023гг проходил обучение по курсу AspNet core Otus.</li>
